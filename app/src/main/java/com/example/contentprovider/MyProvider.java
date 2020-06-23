ab630e50189ac0d6b50852d1f11dcfc018b5b3a3
HungPain69 <hungpain69@gmail.com>
2020-06-24 00:26:13 +0700

    Declare provider tag in manifest, declare URL, Uri, UriMatcher, create database

---
+++ app/src/main/java/com/example/contentprovider/MyProvider.java
@@ -0,0 +1,103 @@
+package com.example.contentprovider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.HashMap;
+
+public class MyProvider extends ContentProvider {
+    static final String PROVIDER_NAME = "com.example.contentprovider.MyProvider";
+    static final String URL = "content://" + PROVIDER_NAME + "/notes";
+    static final Uri CONTENT_URI = Uri.parse(URL);
+
+    static final String _ID = "_id";
+    static final String TITLE = "title";
+    static final String CONTENT = "content";
+
+    private static HashMap<String, String> NOTES_PROJECTION_MAP;
+
+    static final int URI_ALL_ITEMS_CODE  = 1;
+    static final int URI_ONE_ITEM_CODE = 2;
+
+    static final UriMatcher uriMatcher;
+    static{
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "students", URI_ALL_ITEMS_CODE );
+        uriMatcher.addURI(PROVIDER_NAME, "students/#", URI_ONE_ITEM_CODE);
+    }
+    private SQLiteDatabase db;
+    static final String DATABASE_NAME = "DatabaseName";
+    static final String TABLE_NAME = "note_table";
+    static final int DATABASE_VERSION = 1;
+    static final String CREATE_DB_TABLE =
+            " CREATE TABLE " + TABLE_NAME +
+                    " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " title TEXT NOT NULL, " +
+                    " content TEXT NOT NULL);";
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
+
+        /**
+         * Create a write able database which will trigger its
+         * creation if it doesn't already exist.
+         */
+
+        db = dbHelper.getWritableDatabase();
+        return (db == null)? false:true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context){
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_DB_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+    }
+}
